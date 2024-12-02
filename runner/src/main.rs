65a8c047c37972c196e0509e8f1b1d1ba00d1031
nikthechampiongr <nikvog4@gmail.com>
2024-12-02 03:06:17 +0200

    Add runner for days

---
+++ runner/src/main.rs
@@ -0,0 +1,33 @@
+use std::io::Write;
+
+fn main() {
+    print!("Please enter day:");
+    std::io::stdout().flush().unwrap();
+    let mut buf = String::new();
+    std::io::stdin()
+        .read_line(&mut buf)
+        .expect("Failed to read from stdin. How?");
+    let num: u8 = buf.trim().parse().expect("Input is not a number");
+
+    let path = format!("../day_{num}");
+    if !std::fs::exists(&path).expect("Failed to determine whether day exists.") {
+        eprintln!("Day not found.");
+        std::process::exit(1);
+    }
+
+    std::env::set_current_dir(path).unwrap();
+
+    if std::process::Command::new("cargo")
+        .arg("run")
+        .stdout(std::io::stdout())
+        .spawn()
+        .expect("Failed to run solution")
+        .wait()
+        .unwrap()
+        .code()
+        .unwrap()
+        != 0
+    {
+        eprintln!("Running solution returned failure code.");
+    }
+}
